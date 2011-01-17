7a04ffce8ecf4aa050f731152162050e768be65e
pklaus workstation <philipp.klaus@gmail.com>
2011-01-17 20:06:10 +0100

    initial commit of the working tool

---
+++ usleep.c
@@ -0,0 +1,30 @@
+/*
+
+The usleep binary by Philipp Klaus
+
+*/
+
+
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main( int argc, const char* argv[] )
+{
+	int num_usec;
+	
+	// ensure the correct number of parameters are used.
+	if ( argc == 2 ) 
+	{
+		num_usec = atoi( argv[1] );
+		
+		printf( "Sleep for %d microseconds\n", num_usec );
+		usleep(num_usec);
+	} else {
+		printf("help:\nusleep NUM\nNUM is the number of microseconds that you want to sleep.\n");
+	}
+
+}
+
+
+
